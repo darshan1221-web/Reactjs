40b6c2e7ea20a3e976acfc786e223ea26339ea76
darshan1221 <dasuwagheshvari@gmail.com>
2025-12-08 21:04:52 +0530

    constructer in table

--- src/index.js
+++ src/index.js
@@ -4,57 +4,117 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-class Tourcard extends React.Component {
-  render(){
-    return(
+
+class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      thali: 0,
+      roti: 0,
+      chhas: 0,
+      papad: 0,
+      sweet: 0,
+    };
+    this.price = {
+      thali: 200,
+      roti: 20,
+      chhas: 10,
+      papad: 50,
+      sweet: 100,
+    };
+
+    this.name = props.name;
+    this.tableno = props.tableno;
+  }
+
+  updatethali = () => {
+    this.setState({
+      thali: this.state.thali + 1
+    });
+  }
+  updateroti = () => {
+    this.setState({
+      roti: this.state.roti + 1
+    });
+  }
+  updatechhas = () => {
+    this.setState({
+      chhas: this.state.chhas + 1
+    });
+  }
+  updatepapad = () => {
+    this.setState({
+      papad: this.state.papad + 1
+    });
+  }
+  updatesweet = () => {
+    this.setState({
+      sweet: this.state.sweet + 1
+    });
+  }
+   getTotal = () => {
+    const { thali, roti, chhas, papad, sweet } = this.state;
+    const p = this.price;
+    return (
+      thali * p.thali +
+      roti * p.roti +
+      chhas * p.chhas +
+      papad * p.papad +
+      sweet * p.sweet
+    );
+  };
+  
+  render() {
+    return (
       <div className="col-lg-4">
-  <div className="card rounded-5 shadow ">
-    <div className="position-relative    ">
-      {/* Match Title */}
-      <img className="img-fluid  rounded-top-5 " src="https://picsum.photos/600?random=1" alt="location" />
-      <span className="badge bg-dark position-absolute bottom-0 end-0 m-3">
-        ⭐ 4.5/5
-      </span>
-    </div>
-    <div className="card-body d-flex justify-content-between">
-      {/* Additional Info */}
-      <div className="card-title fw-bold">amazing safari adventure
-        <div className="row">
-          <div className="col">
-            <div>
-              KIADB Export promotion
+        <div className="card my-3">
+          <div className="card-header text-bg-danger d-flex justify-content-between">
+            <h5>{this.name}</h5>
+            <h5><span className="badge bg-white text-primary">{this.tableno}</span></h5>
+          </div>
+          <div className="card-body">
+            <button type="button" className="btn btn-primary w-100" onClick={this.updatethali}>Thali - <span className="badge bg-white text-primary">{this.state.thali}</span><span className="badge bg-white text-primary m-1">{this.price.thali}</span></button>
+            <div className="row  mt-2">
+              <div className="col-6 ">
+                <button type="button" className="btn btn-dark w-100" onClick={this.updateroti}>Roti - <span className="badge bg-white text-dark">{this.state.roti}</span><span className="badge bg-white text-primary m-1">{this.price.roti}</span></button>
+              </div>
+              <div className="col-6">
+                <button type="button" className="btn btn-warning w-100" onClick={this.updatechhas}>chhas - <span className="badge bg-white text-dark">{this.state.chhas}</span><span className="badge bg-white text-primary m-1">{this.price.chhas}</span></button>
+              </div>
             </div>
-            <div>
-              industrial area <span>⭐⭐⭐⭐⭐</span>
+            <div className="row mt-2">
+              <div className="col-6">
+                <button type="button" className="btn btn-info w-100" onClick={this.updatepapad}>Papad- <span className="badge bg-white text-dark">{this.state.papad}</span><span className="badge bg-white text-primary m-1">{this.price.papad}</span></button>
+              </div>
+              <div className="col-6">
+                <button type="button" className="btn btn-success w-100" onClick={this.updatesweet}>sweet - <span className="badge bg-white text-dark">{this.state.sweet}</span><span className="badge bg-white text-primary m-1">{this.price.sweet}</span></button>
+              </div>
             </div>
           </div>
+          <div className="card-footer d-flex justify-content-between">
+            <span className="h5">Total</span>
+            <span className="h5">₹ {this.getTotal()}</span>
+          </div>
         </div>
-      </div>
-      <div className="price"><span className="fw-bold">$5000</span>
-        <p>per night</p>
+      </div>)
+
+  }
+}
+class Restaurant extends React.Component {
+  render() {
+    return (<div className="container">
+      <div className="row">
+        <Table name="darshan" tableno="01" />
+        <Table name="swati" tableno="02" />
+        <Table name="abhishek" tableno="03" />
+
       </div>
     </div>
-  </div>
-</div>
+
     )
   }
 }
-class Tourdetails extends React.Component {
-  render() {
-    return(
-<div className="container my-5">
-  <div className="row justify-content-between">
-    <Tourcard />
-    <Tourcard />
-    <Tourcard />
-    <Tourcard />
-  </div>
-</div>
 
-    )
-  }
-}  
-
-root.render(<Tourdetails />);
+root.render(<Restaurant />);
 
 
